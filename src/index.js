55142434167c6405d1eebbe1bb1e9509f35fc28a
Freakin_D1A <d1adewanasad@gmail.com>
2024-01-20 06:50:31 +0600

    app

--- src/index.js
+++ src/index.js
@@ -1,32 +1,19 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./env" });
 import connectDB from "./db/index.js";
-import express from "express";
+import app from "./app.js";
+import { COLORS } from "./constant.js";
 
-connectDB();
-const app = express();
-
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}`);
-//     app.on("error", (error) => {
-//       console.log(COLORS.FgRed);
-//       console.log("\n\n\n");
-//       console.log("Error", error);
-//       console.log(COLORS.Reset);
-//     });
-
-//     app.listen(process.env.PORT, () => {
-//       console.log(COLORS.FgCyan, '\n\n\n');
-//       console.log("\n\n\n");
-//       console.log(`App is listening on port ${process.env.PORT}`);
-//       console.log(COLORS.Reset, '\n\n\n');
-//     });
-//   } catch (error) {
-//     console.log(COLORS.FgRed);
-//     console.log("\n\n\n");
-//     console.log("Error", error);
-//     console.log(COLORS.Reset);
-//     throw err;
-//   }
-// })();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(COLORS.FgCyan, "\n\n\n");
+      console.log(`App is listening on port ${process.env.PORT || 8000}`);
+      console.log(COLORS.Reset, "\n\n\n");
+    });
+  })
+  .catch((error) => {
+    console.log(COLORS.FgRed, "\n\n");
+    console.log("Mongodb connection error Error:\n", error);
+    console.log(COLORS.Reset, "\n\n");
+  });
